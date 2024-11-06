c74753289d378408dfe195fd8102002d3af9d922
reginaguermandi <reginaguermandi@gmail.com>
2024-11-06 14:00:41 -0300

    feat: add 10 year validation to middlweare and import to service

--- src/controllers/CarsController.js
+++ src/controllers/CarsController.js
@@ -1,19 +1,29 @@
 const carService = require("../services/cars");
 
 module.exports = {
+	async findAll(req, res) {
+		try {
+			const result = await carService.getAll();
+			res.status(200).send(result);
+		} catch (error) {
+			return res.status(500).send({ error: error.message });
+		}
+	},
+
 	async create(req, res) {
 		try {
 			const { brand, model, year, plate } = req.body;
-
 			const newCar = await carService.createCar({
 				brand,
 				model,
 				year,
 				plate,
 			});
-
 			return res.status(201).send(newCar);
 		} catch (error) {
+			if (error.name === "ValidationError") {
+				return res.status(400).send({ error: error.message });
+			}
 			return res.status(500).send({ error: error.message });
 		}
 	},
--- src/services/cars.js
+++ src/services/cars.js
@@ -1,7 +1,16 @@
 const carRepository = require("../repository/cars");
+const ValidationError = require("../middleware/post_entry_validation");
+const maxYearLimit = require("../middleware/year_validation");
 
 module.exports = {
 	async createCar(carData) {
+		if (!carData.brand) throw new ValidationError("brand is required");
+		if (!carData.model) throw new ValidationError("model is required");
+		if (!carData.year) throw new ValidationError("year is required");
+		if (!carData.plate) throw new ValidationError("plate is required");
+		if (!maxYearLimit(carData.year))
+			throw new ValidationError("year must be between 2015 and 2025");
+
 		const newCar = await carRepository.insertCar(carData);
 		return newCar;
 	},
